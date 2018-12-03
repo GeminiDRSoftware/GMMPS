8a9ea2132d7ccaff220c23e39b150963dcffc9ea
Bryan Miller <millerwbryan@gmail.com>
2018-12-03 14:53:40 -0300

    Fix library search order

--- scripts/locate_libs.sh
+++ scripts/locate_libs.sh
@@ -18,15 +18,16 @@ OS=$1
 if [ $OS = "Darwin" ]; then
     libsuffix=".dylib"
     mdfind -name libskycat3.1.4.dylib > libsfound_$$
-    mdfind -name libskycat3.1.3.dylib > libsfound_$$
+    mdfind -name libskycat3.1.3.dylib >> libsfound_$$
     mdfind -name libskycat3.1.2.dylib >> libsfound_$$
 else
     libsuffix=".so"
     locate -e -b '\libskycat3.1.4.so' > libsfound_$$
-    locate -e -b '\libskycat3.1.3.so' > libsfound_$$
+    locate -e -b '\libskycat3.1.3.so' >> libsfound_$$
     locate -e -b '\libskycat3.1.2.so' >> libsfound_$$
 fi
 
+cat libsfound_$$
 
 ############################################
 # How many libs were found? Leave if none
